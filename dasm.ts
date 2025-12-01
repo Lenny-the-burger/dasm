42bcd881492e841a0904b05e621e38bd0d952819
Lenny the burger <tednboss@gmail.com>
2025-12-01 00:25:24 -0500

    ghjkl;

--- dasm.ts
+++ dasm.ts
@@ -2,15 +2,51 @@
 import { simd, relaxedSimd } from "../node_modules/wasm-feature-detect/dist/esm/index.js";
 
 enum SectionType {
+    NONE,
     DATA,
     FUCNTIONS,
     PROGRAM
 }
 
-const data_types = ["num", "vec2", "vec3", "vecn", "any"];
+enum DataType {
+    NUM = "num",
+    VEC2 = "vec2",
+    VEC3 = "vec3",
+    VECN = "vecn",
+    ANY = "any"
+}
+
+// These are actually the only three data types. Everything else is just template declarations.
+type dataNum = {
+    value: number;
+}
+
+type dataVec2 = {
+    value: [number, number];
+}
+
+type dataVec3 = {
+    value: [number, number, number];
+}
+
+// regular vs list variables
+type singletDataVar<T> = {
+    name: string;
+    value: T;
+}
+
+type dataListVar<T> = {
+    name: string;
+    values: T[];
+}
 
 // the dasm runtime and compiler
 class dasm_runtime {
+
+    public compiler_options = {
+        // dump data values into stdout once read
+        showDataVals: false
+    };
 
     public input_text: string = "default input";
 
@@ -18,6 +54,28 @@ class dasm_runtime {
     public wat_code: string = "default wat";
 
     public stdout: string = "";
+    private latest_compile_start_time: Date;
+
+    public data_singlets: Map<string, singletDataVar<any>> = new Map();
+    public data_lists: Map<string, dataListVar<any>> = new Map();
+
+    reset_compiler_state() {
+        this.dasm_code = "";
+        this.wat_code = "";
+        this.data_singlets.clear();
+        this.data_lists.clear();
+    }
+
+    print_data_vars() {
+        this.cinfo("Data Singlet Variables:");
+        this.data_singlets.forEach((value, key) => {
+            this.cout(" - " + key + " = " + JSON.stringify(value.value) + "<br>");
+        });
+        this.cinfo("Data List Variables:");
+        this.data_lists.forEach((value, key) => {
+            this.cout(" - " + key + " = " + JSON.stringify(value.values) + "<br>");
+        });
+    }
 
     get_dasm(): string {
         return this.dasm_code;
@@ -47,6 +105,12 @@ class dasm_runtime {
         this.stdout += msg;
     }
 
+    compile_failed(err: string): never {
+        this.cerr(err);
+        this.cinfo("Failed to compile in " + (new Date().getTime() - this.latest_compile_start_time.getTime()) + " ms");
+        throw new Error(err);
+    }
+
     constructor() {
         // get supported wasm extensions
         if (simd()) {
@@ -63,14 +127,26 @@ class dasm_runtime {
     }
 
     compile(input: string) {
+        this.reset_compiler_state();
+        this.latest_compile_start_time = new Date();
         this.input_text = input;
 
-        this.cinfo("<br>Started compiling at " + new Date().toLocaleTimeString());
+        this.cinfo("<br>Started compiling at " + this.latest_compile_start_time.toLocaleTimeString());
 
-        var current_section: SectionType = SectionType.DATA;
+        var current_section: SectionType = SectionType.NONE;
 
         // loop over every line
         const lines = input.split("\n");
+
+        // if first line starts with # thats means compiler options (not a code line)
+        if (lines.length > 0 && lines[0].startsWith("#")) {
+            const options_list = lines[0].slice(1).trim().toLocaleLowerCase().split(" ");
+
+            if (options_list.includes("dd") || options_list.includes("dumpdata")) this.compiler_options.showDataVals = true;
+
+            lines.shift();
+        }
+
         var line_number = -1;
         for (let line of lines) {
             line_number += 1;
@@ -80,24 +156,170 @@ class dasm_runtime {
             if (line.length == 0) continue;
             if (line.startsWith("//")) continue;
 
-            // await section
-            if (!line.startsWith("@")) {
-                continue;
-            }
-
             switch (current_section) {
-                case SectionType.DATA:
-                    if (!line.toLocaleLowerCase().startsWith("@data")) {
-                        this.cerr("Unknown section: " + line + " at line " + line_number + ", must begin with data section");
-                        return;
-                    } else {
-                        // we are in the data section
+                case SectionType.NONE:
+                    if (!line.startsWith("@")) {
                         continue;
                     }
 
-                    var words = line.split(" ");
+                    if (!line.toLocaleLowerCase().startsWith("@data")) {
+                        this.compile_failed("Unknown section: '" + line + "' at line " + line_number + ", must begin with data section");
+                    } else {
+                        // we are in data section
+                        current_section = SectionType.DATA;
+                        continue;
+                    }
+
+                case SectionType.DATA:
+                    // Data should only contain variable declarations
+                    if (!line.startsWith("@")) {
+                        this.parseDataVar(line, line_number);
+                    } else {
+                        // next should be the functions section
+                        if (!line.toLocaleLowerCase().startsWith("@functions")) {
+                            this.compile_failed("Unknown section: '" + line + "' at line " + line_number + ", expected functions section");
+                        } else {
+                            current_section = SectionType.FUCNTIONS;
+                            if (this.compiler_options.showDataVals) this.print_data_vars();
+                            continue;
+                        }
+                    }
+
             }
         }
+    }
+
+    parseDataVar(line: string, line_num: number) {
+        const tokens = line.split(" ");
+        // Declarations should be at least <data type> <name> = <value>
+        if (tokens.length < 4) this.compile_failed("Incomplete variable declaration: '" + line + "' at line " + line_num);
+
+        // any and vecn cannot be used for variables, only function parameters
+        if (tokens[0] == DataType.ANY || tokens[0] == DataType.VECN) this.compile_failed("Invalid data type for variable declaration: '" + tokens[0] + "' at line " + line_num);
+
+        // if the type ends with [], it's a list variable
+        if (tokens[0].endsWith("[]")) {
+            this.parseDataListVar(tokens, line_num);
+        } else {
+            this.parseDataSingletVar(tokens, line_num);
+        }
+
+        // eventually you will also be able to load from a datafile for big lists
+    }
+
+    parseDataSingletVar(tokens: string[], line_num: number) {
+        const merged_tokens = tokens.slice(3).join("");
+        const var_name = tokens[1];
+        if (this.data_lists.has(var_name)) this.compile_failed("Variable redifinition: '" + var_name + "' at line " + line_num);
+
+        switch (tokens[0]) {
+            case DataType.NUM:
+                const value = this.parseNumLiteral(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value });
+                break;
+
+            // for vec3 and vec3 we have to join all tokens after the equal sign in case there are spaces
+            case DataType.VEC2:
+                const value2 = this.parseVec2Literal(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value2 });
+                break;
+
+            case DataType.VEC3:
+                const value3 = this.parseVec3Literal(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value3 });
+                break;
+
+            default:
+                this.compile_failed("Unknown data type: '" + tokens[0] + "' at line " + line_num);
+        }
+    }
+
+    parseDataListVar(tokens: string[], line_num: number) {
+        const merged_tokens = tokens.slice(3).join("");
+        const values_str = merged_tokens.replace(/\s/g, ''); // remove all whitespace
+        const var_name = tokens[1];
+
+        if (this.data_lists.has(var_name)) this.compile_failed("Variable redifinition: '" + var_name + "' at line " + line_num);
+
+        // remove the [] from the type
+        const base_type = tokens[0].slice(0, -2);
+        switch (base_type) {
+            case DataType.NUM:
+                
+                const value_tokens_num = values_str.split(",");
+                const values: dataNum[] = [];
+                for (let vt of value_tokens_num) {
+                    const val = this.parseNumLiteral(vt, line_num);
+                    values.push(val);
+                }
+
+                this.data_lists.set(var_name, { name: var_name, values: values });
+                break;
+
+            case DataType.VEC2:
+                // this one is a bit trickier since vec2 literals have commas inside them
+                const value_tokens_vec2 = values_str.slice(2, -2).split("),(").map(s => `(${s})`); // remove the square brackets, split by "),(", and then add back the parentheses
+                const values2: dataVec2[] = [];
+                for (let vt of value_tokens_vec2) {
+                    const val = this.parseVec2Literal(vt, line_num);
+                    values2.push(val);
+                }
+
+                this.data_lists.set(var_name, { name: var_name, values: values2 });
+                break;
+
+            case DataType.VEC3:
+                const value_tokens_vec3 = values_str.slice(2, -2).split("),(").map(s => `(${s})`);
+                const values3: dataVec3[] = [];
+                for (let vt of value_tokens_vec3) {
+                    const val = this.parseVec3Literal(vt, line_num);
+                    values3.push(val);
+                }
+
+                this.data_lists.set(var_name, { name: var_name, values: values3 });
+                break;
+
+            default:
+                this.compile_failed("Unknown data type: '" + base_type + "' at line " + line_num);
+        }
+    }
+
+    // Should be in the format of [0-9]*\.?[0-9]*
+    parseNumLiteral(token: string, line_num: number): dataNum {
+        const value = Number(token);
+        if (isNaN(value)) this.compile_failed("Error parsing num literal: '" + token + "' at line " + line_num);
+        return { value: value };
+    }
+
+    // should be in the format of (num,num)
+    parseVec2Literal(token: string, line_num: number): dataVec2 {
+        if (!token.startsWith("(") || !token.endsWith(")")) this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+
+        const content = token.slice(1, -1);
+        const parts = content.split(",");
+        if (parts.length != 2) this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+
+        const x = Number(parts[0]);
+        const y = Number(parts[1]);
+        if (isNaN(x) || isNaN(y)) this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+
+        return { value: [x, y] };
+    }
+
+    // should be in the format of (num,num,num)
+    parseVec3Literal(token: string, line_num: number): dataVec3 {
+        if (!token.startsWith("(") || !token.endsWith(")")) this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+
+        const content = token.slice(1, -1);
+        const parts = content.split(",");
+        if (parts.length != 3) this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+
+        const x = Number(parts[0]);
+        const y = Number(parts[1]);
+        const z = Number(parts[2]);
+        if (isNaN(x) || isNaN(y) || isNaN(z)) this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+
+        return { value: [x, y, z] };
     }
 }
 
--- dist/dasm.js
+++ dist/dasm.js
@@ -2,13 +2,37 @@
 import { simd, relaxedSimd } from "../node_modules/wasm-feature-detect/dist/esm/index.js";
 var SectionType;
 (function (SectionType) {
-    SectionType[SectionType["DATA"] = 0] = "DATA";
-    SectionType[SectionType["FUCNTIONS"] = 1] = "FUCNTIONS";
-    SectionType[SectionType["PROGRAM"] = 2] = "PROGRAM";
+    SectionType[SectionType["NONE"] = 0] = "NONE";
+    SectionType[SectionType["DATA"] = 1] = "DATA";
+    SectionType[SectionType["FUCNTIONS"] = 2] = "FUCNTIONS";
+    SectionType[SectionType["PROGRAM"] = 3] = "PROGRAM";
 })(SectionType || (SectionType = {}));
-const data_types = ["num", "vec2", "vec3", "vecn", "any"];
+var DataType;
+(function (DataType) {
+    DataType["NUM"] = "num";
+    DataType["VEC2"] = "vec2";
+    DataType["VEC3"] = "vec3";
+    DataType["VECN"] = "vecn";
+    DataType["ANY"] = "any";
+})(DataType || (DataType = {}));
 // the dasm runtime and compiler
 class dasm_runtime {
+    reset_compiler_state() {
+        this.dasm_code = "";
+        this.wat_code = "";
+        this.data_singlets.clear();
+        this.data_lists.clear();
+    }
+    print_data_vars() {
+        this.cinfo("Data Singlet Variables:");
+        this.data_singlets.forEach((value, key) => {
+            this.cout(" - " + key + " = " + JSON.stringify(value.value) + "<br>");
+        });
+        this.cinfo("Data List Variables:");
+        this.data_lists.forEach((value, key) => {
+            this.cout(" - " + key + " = " + JSON.stringify(value.values) + "<br>");
+        });
+    }
     get_dasm() {
         return this.dasm_code;
     }
@@ -30,11 +54,22 @@ class dasm_runtime {
     cout(msg) {
         this.stdout += msg;
     }
+    compile_failed(err) {
+        this.cerr(err);
+        this.cinfo("Failed to compile in " + (new Date().getTime() - this.latest_compile_start_time.getTime()) + " ms");
+        throw new Error(err);
+    }
     constructor() {
+        this.compiler_options = {
+            // dump data values into stdout once read
+            showDataVals: false
+        };
         this.input_text = "default input";
         this.dasm_code = "default dasm";
         this.wat_code = "default wat";
         this.stdout = "";
+        this.data_singlets = new Map();
+        this.data_lists = new Map();
         // get supported wasm extensions
         if (simd()) {
             this.cinfo("SIMD in wasm is supported");
@@ -50,11 +85,20 @@ class dasm_runtime {
         }
     }
     compile(input) {
+        this.reset_compiler_state();
+        this.latest_compile_start_time = new Date();
         this.input_text = input;
-        this.cinfo("<br>Started compiling at " + new Date().toLocaleTimeString());
-        var current_section = SectionType.DATA;
+        this.cinfo("<br>Started compiling at " + this.latest_compile_start_time.toLocaleTimeString());
+        var current_section = SectionType.NONE;
         // loop over every line
         const lines = input.split("\n");
+        // if first line starts with # thats means compiler options (not a code line)
+        if (lines.length > 0 && lines[0].startsWith("#")) {
+            const options_list = lines[0].slice(1).trim().toLocaleLowerCase().split(" ");
+            if (options_list.includes("dd") || options_list.includes("dumpdata"))
+                this.compiler_options.showDataVals = true;
+            lines.shift();
+        }
         var line_number = -1;
         for (let line of lines) {
             line_number += 1;
@@ -63,23 +107,155 @@ class dasm_runtime {
                 continue;
             if (line.startsWith("//"))
                 continue;
-            // await section
-            if (!line.startsWith("@")) {
-                continue;
-            }
             switch (current_section) {
-                case SectionType.DATA:
-                    if (!line.toLocaleLowerCase().startsWith("@data")) {
-                        this.cerr("Unknown section: " + line + " at line " + line_number + ", must begin with data section");
-                        return;
-                    }
-                    else {
-                        // we are in the data section
+                case SectionType.NONE:
+                    if (!line.startsWith("@")) {
                         continue;
                     }
-                    var words = line.split(" ");
+                    if (!line.toLocaleLowerCase().startsWith("@data")) {
+                        this.compile_failed("Unknown section: '" + line + "' at line " + line_number + ", must begin with data section");
+                    }
+                    else {
+                        // we are in data section
+                        current_section = SectionType.DATA;
+                        continue;
+                    }
+                case SectionType.DATA:
+                    // Data should only contain variable declarations
+                    if (!line.startsWith("@")) {
+                        this.parseDataVar(line, line_number);
+                    }
+                    else {
+                        // next should be the functions section
+                        if (!line.toLocaleLowerCase().startsWith("@functions")) {
+                            this.compile_failed("Unknown section: '" + line + "' at line " + line_number + ", expected functions section");
+                        }
+                        else {
+                            current_section = SectionType.FUCNTIONS;
+                            if (this.compiler_options.showDataVals)
+                                this.print_data_vars();
+                            continue;
+                        }
+                    }
             }
         }
+    }
+    parseDataVar(line, line_num) {
+        const tokens = line.split(" ");
+        // Declarations should be at least <data type> <name> = <value>
+        if (tokens.length < 4)
+            this.compile_failed("Incomplete variable declaration: '" + line + "' at line " + line_num);
+        // any and vecn cannot be used for variables, only function parameters
+        if (tokens[0] == DataType.ANY || tokens[0] == DataType.VECN)
+            this.compile_failed("Invalid data type for variable declaration: '" + tokens[0] + "' at line " + line_num);
+        // if the type ends with [], it's a list variable
+        if (tokens[0].endsWith("[]")) {
+            this.parseDataListVar(tokens, line_num);
+        }
+        else {
+            this.parseDataSingletVar(tokens, line_num);
+        }
+        // eventually you will also be able to load from a datafile for big lists
+    }
+    parseDataSingletVar(tokens, line_num) {
+        const merged_tokens = tokens.slice(3).join("");
+        const var_name = tokens[1];
+        if (this.data_lists.has(var_name))
+            this.compile_failed("Variable redifinition: '" + var_name + "' at line " + line_num);
+        switch (tokens[0]) {
+            case DataType.NUM:
+                const value = this.parseNumLiteral(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value });
+                break;
+            // for vec3 and vec3 we have to join all tokens after the equal sign in case there are spaces
+            case DataType.VEC2:
+                const value2 = this.parseVec2Literal(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value2 });
+                break;
+            case DataType.VEC3:
+                const value3 = this.parseVec3Literal(merged_tokens.replace(/\s/g, ''), line_num);
+                this.data_singlets.set(var_name, { name: var_name, value: value3 });
+                break;
+            default:
+                this.compile_failed("Unknown data type: '" + tokens[0] + "' at line " + line_num);
+        }
+    }
+    parseDataListVar(tokens, line_num) {
+        const merged_tokens = tokens.slice(3).join("");
+        const values_str = merged_tokens.replace(/\s/g, ''); // remove all whitespace
+        const var_name = tokens[1];
+        if (this.data_lists.has(var_name))
+            this.compile_failed("Variable redifinition: '" + var_name + "' at line " + line_num);
+        // remove the [] from the type
+        const base_type = tokens[0].slice(0, -2);
+        switch (base_type) {
+            case DataType.NUM:
+                const value_tokens_num = values_str.split(",");
+                const values = [];
+                for (let vt of value_tokens_num) {
+                    const val = this.parseNumLiteral(vt, line_num);
+                    values.push(val);
+                }
+                this.data_lists.set(var_name, { name: var_name, values: values });
+                break;
+            case DataType.VEC2:
+                // this one is a bit trickier since vec2 literals have commas inside them
+                const value_tokens_vec2 = values_str.slice(2, -2).split("),(").map(s => `(${s})`); // remove the square brackets, split by "),(", and then add back the parentheses
+                const values2 = [];
+                for (let vt of value_tokens_vec2) {
+                    const val = this.parseVec2Literal(vt, line_num);
+                    values2.push(val);
+                }
+                this.data_lists.set(var_name, { name: var_name, values: values2 });
+                break;
+            case DataType.VEC3:
+                const value_tokens_vec3 = values_str.slice(2, -2).split("),(").map(s => `(${s})`);
+                const values3 = [];
+                for (let vt of value_tokens_vec3) {
+                    const val = this.parseVec3Literal(vt, line_num);
+                    values3.push(val);
+                }
+                this.data_lists.set(var_name, { name: var_name, values: values3 });
+                break;
+            default:
+                this.compile_failed("Unknown data type: '" + base_type + "' at line " + line_num);
+        }
+    }
+    // Should be in the format of [0-9]*\.?[0-9]*
+    parseNumLiteral(token, line_num) {
+        const value = Number(token);
+        if (isNaN(value))
+            this.compile_failed("Error parsing num literal: '" + token + "' at line " + line_num);
+        return { value: value };
+    }
+    // should be in the format of (num,num)
+    parseVec2Literal(token, line_num) {
+        if (!token.startsWith("(") || !token.endsWith(")"))
+            this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+        const content = token.slice(1, -1);
+        const parts = content.split(",");
+        if (parts.length != 2)
+            this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+        const x = Number(parts[0]);
+        const y = Number(parts[1]);
+        if (isNaN(x) || isNaN(y))
+            this.compile_failed("Error parsing vec2 literal: '" + token + "' at line " + line_num);
+        return { value: [x, y] };
+    }
+    // should be in the format of (num,num,num)
+    parseVec3Literal(token, line_num) {
+        if (!token.startsWith("(") || !token.endsWith(")"))
+            this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+        const content = token.slice(1, -1);
+        const parts = content.split(",");
+        if (parts.length != 3)
+            this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+        const x = Number(parts[0]);
+        const y = Number(parts[1]);
+        const z = Number(parts[2]);
+        if (isNaN(x) || isNaN(y) || isNaN(z))
+            this.compile_failed("Error parsing vec3 literal: '" + token + "' at line " + line_num);
+        return { value: [x, y, z] };
     }
 }
 export { dasm_runtime };
